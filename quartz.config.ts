6223d47bcd6fcf6a0e721e869ef5c56b33a11bf5
marchelo2212 <marchelo2212@gmail.com>
2025-12-01 10:43:23 -0500

    Cambios locales en Quartz antes de deploy

--- quartz.config.ts
+++ quartz.config.ts
@@ -66,7 +66,11 @@ const config: QuartzConfig = {
         },
         keepBackground: false,
       }),
-      Plugin.ObsidianFlavoredMarkdown({ enableInHtmlEmbed: true }),
+      Plugin.ObsidianFlavoredMarkdown({ 
+        enableInHtmlEmbed: true,
+        enableYouTubeEmbed: true,
+        enableVideoEmbed: true,
+        }),
       Plugin.GitHubFlavoredMarkdown(),
       Plugin.TableOfContents(),
       Plugin.CrawlLinks({ markdownLinkResolution: "shortest" }),
